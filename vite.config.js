3b6c921f6a7e8a22d64dc99cb5e3816ea9ac798b
olivertoussaint <toussaint.olivier66@gmail.com>
2024-06-28 00:30:51 +0200

    create alias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins:[react()],
   resolve: {
     alias: [
       {
@@ -15,8 +14,36 @@ export default defineConfig({
         replacement: path.resolve(__dirname, 'src')
       },
       {
+        find: '@icons',
+        replacement: path.resolve(__dirname, 'src/assets/icons')
+      },
+      {
+        find: '@img',
+        replacement: path.resolve(__dirname, 'src/assets/img')
+      },
+      {
+        find: '@layout',
+        replacement: path.resolve(__dirname, 'src/components/layout')
+      },
+      {
+        find: '@partials',
+        replacement: path.resolve(__dirname, 'src/components/partials')
+      },
+      {
         find: '@home',
-        
+        replacement: path.resolve(__dirname, 'src/pages/Home/index.jsx')
+      },
+      {
+        find: '@login',
+        replacement: path.resolve(__dirname, 'src/pages/Login/index.jsx')
+      },
+      {
+        find: '@profile',
+        replacement: path.resolve(__dirname, 'src/pages/Profile/index.jsx')
+      },
+      {
+        find: '@error',
+        replacement: path.resolve(__dirname, 'src/pages/Error/Error.jsx')
       }
     ]
   }
