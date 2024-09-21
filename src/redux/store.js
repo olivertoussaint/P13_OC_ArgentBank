879f0e74975aa7d359da614ec4fb428165b7b962
olivertoussaint <toussaint.olivier66@gmail.com>
2024-09-21 16:58:14 +0200

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { authReducer } from './reducers/auth.reducer'
-import { userReducer } from './reducers/user.reducer'
-
-const rootReducer = combineReducers({
-    auth: authReducer,
-    user: userReducer
-})
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
+import authReducer from './slices/authSlice'
 
 const store = configureStore({
-    reducer: rootReducer,
-    devTools: true
-})
-export default store
+  reducer: {
+    user: userReducer,
+    auth: authReducer,
+  },
+
+});
+
+export default store;
