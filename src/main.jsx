049c73cf91e303d5af9392f1ffa0e6ac7e318188
olivertoussaint <toussaint.olivier66@gmail.com>
2024-09-21 16:58:47 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import { fetchUserProfile } from './redux/slices/userSlice';
+
+// Vérification si l'utilisateur est authentifié et a cliqué sur "Remember Me"
+const token = localStorage.getItem('token'); // Vérifie le token dans localStorage
+if (token) {
+  store.dispatch(fetchUserProfile(token)); // Récupérer le profil utilisateur uniquement si le token est présent
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
-    </React.StrictMode>
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
