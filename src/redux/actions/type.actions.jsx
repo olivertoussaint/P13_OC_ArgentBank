6c895718ea5015e3c3a477e8425c5428d501f246
olivertoussaint <toussaint.olivier66@gmail.com>
2024-09-21 17:03:44 +0200

    Remove deprecated files after code refactor

--- src/redux/actions/type.actions.jsx
+++
@@ -1,6 +0,0 @@
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
-export const LOGIN_FAIL = "LOGIN_FAIL"
-export const LOGOUT = "LOGOUT"
-
-export const GET_USERPROFILE = "GET_USERPROFILE"
-export const EDIT_USERNAME = "EDIT_USERNAME"
