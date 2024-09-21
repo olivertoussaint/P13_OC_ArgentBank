8d807e3f63c6d54544ab2aa34a9495088ce2d4ae
olivertoussaint <toussaint.olivier66@gmail.com>
2024-09-21 16:46:21 +0200

    for better global state management in a React application using Redux

---
+++ src/redux/selectors/user.selectors.js
@@ -0,0 +1,4 @@
+export const selectUserData = (state) => state.user.userProfile;
+export const selectUserStatus = (state) => state.user.status;
+export const selectUserError = (state) => state.user.error;
+
