cd6437a44b0e73dcb0818fc36941f2bf145df940
olivertoussaint <toussaint.olivier66@gmail.com>
2024-11-02 13:50:40 +0100

    add a spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="loading-circle"></div>
+    </div>
+  );
+};
+
+export default Spinner;
